de556efa70e3613d0c1aae251e53efea8a95b5dc
Abin Jose <joseabin291@gmail.com>
2025-11-19 15:35:48 +0530

    admin template added

--- frontend/src/routing/app-routes.jsx
+++ frontend/src/routing/app-routes.jsx
@@ -1,9 +1,13 @@
 import { Routes, Route } from "react-router-dom";
+import { publicUser } from "../globals/route-names";
 
 import PublicUserLayout from "../layouts/public-user-layout";
 import EmployerLayout from "../layouts/employer-layout";
 import CandidateLayout from "../layouts/candidate-layout";
 import { base } from "../globals/route-names";
+import AdminLoginPage from "../app/pannels/public-user/components/pages/admin-login";
+import AdminLayout from "../layouts/admin-layout";
+
 
 function AppRoutes() {
     return (
@@ -11,6 +15,10 @@ function AppRoutes() {
             <Route path={base.PUBLIC_PRE + "/*"} element={<PublicUserLayout />} />
             <Route path={base.EMPLOYER_PRE + "/*"} element={<EmployerLayout />} />
             <Route path={base.CANDIDATE_PRE + "/*"} element={<CandidateLayout />} />
+            <Route path={base.ADMIN_PRE + "/*"} element={<AdminLayout />} />
+
+            <Route path={publicUser.pages.ADMIN_LOGIN} element={<AdminLoginPage />} />
+
         </Routes>
     )
 }
